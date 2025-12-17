a11387f9620dce3b085173a0f02990783cb7ae1a
Zach Smith <zachdsmith2015@gmail.com>
2025-12-17 11:33:14 -0800

    fix: rollback txt record verification

--- internal/controller/conditions.go
+++ internal/controller/conditions.go
@@ -1,17 +1,13 @@
 package controller
 
 const (
-	CondAccepted                       = "Accepted"
-	CondProgrammed                     = "Programmed"
-	CondDiscovered                     = "Discovered"
-	CondVerificationRecord             = "VerificationRecord"
-	ReasonAccepted                     = "Accepted"
-	ReasonPending                      = "Pending"
-	ReasonInvalidDNSRecordSet          = "InvalidDNSRecordSet"
-	ReasonProgrammed                   = "Programmed"
-	ReasonDiscovered                   = "Discovered"
-	ReasonDNSZoneInUse                 = "DNSZoneInUse"
-	ReasonVerificationRecordPending    = "VerificationRecordPending"
-	ReasonVerificationRecordProgrammed = "VerificationRecordProgrammed"
-	ReasonVerificationDomainVerified   = "DomainAlreadyVerified"
+	CondAccepted              = "Accepted"
+	CondProgrammed            = "Programmed"
+	CondDiscovered            = "Discovered"
+	ReasonAccepted            = "Accepted"
+	ReasonPending             = "Pending"
+	ReasonInvalidDNSRecordSet = "InvalidDNSRecordSet"
+	ReasonProgrammed          = "Programmed"
+	ReasonDiscovered          = "Discovered"
+	ReasonDNSZoneInUse        = "DNSZoneInUse"
 )
